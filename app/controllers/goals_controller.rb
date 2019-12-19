2a5782276ef87f8bfaab545094faba8d1961df84
hikaaaaa <i.hikaru.12225@gmail.com>
2019-12-20 08:42:20 +0900

    finish 2-12

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,58 +1,56 @@
 class GoalsController < ApplicationController
-  before_action :set_goal, only: [:show, :edit, :update, :destroy]
-
-  # GET /goals
-  def index
-    @goals = Goal.all
-  end
-
-  # GET /goals/1
-  def show
-  end
-
-  # GET /goals/new
-  def new
-    @goal = Goal.new
-  end
-
-  # GET /goals/1/edit
-  def edit
-  end
-
-  # POST /goals
-  def create
-    @goal = Goal.new(goal_params)
-
-    if @goal.save
-      redirect_to @goal, notice: 'Goal was successfully created.'
-    else
-      render :new
+    before_action :authenticate_user!
+    before_action :set_goal, only: [:show, :edit, :update, :destroy]
+  
+    # GET /goals
+    def index
+      @goals = current_user.goals.all
     end
-  end
-
-  # PATCH/PUT /goals/1
-  def update
-    if @goal.update(goal_params)
-      redirect_to @goal, notice: 'Goal was successfully updated.'
-    else
-      render :edit
+  
+  
+    # GET /goals/new
+    def new
+      @goal = current_user.goals.new
     end
-  end
+  
+    # GET /goals/1/edit
+    def edit
+    end
+  
+    # POST /goals
+    def create
 
-  # DELETE /goals/1
-  def destroy
-    @goal.destroy
-    redirect_to goals_url, notice: 'Goal was successfully destroyed.'
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
+    @goal = current_user.goals.new(goal_params)
+      if @goal.save
+        @status = true
+      else
+        @status = false
+      end
+    end
+  
+    # PATCH/PUT /goals/1
+    def update
+      if @goal.update(goal_params)
+        @status = true
+      else
+        @status = false
+      end
+    end
+  
+    # DELETE /goals/1
+    def destroy
+      @goal.destroy
+    end
+  
+    private
+  
     def set_goal
-      @goal = Goal.find(params[:id])
+      @goal = current_user.goals.find_by(id: params[:id])
+      redirect_to(goals_url, alert: "ERROR!!") if @goal.blank?
     end
-
-    # Only allow a trusted parameter "white list" through.
+  
     def goal_params
       params.require(:goal).permit(:title, :user_id)
     end
-end
+  end
+  
